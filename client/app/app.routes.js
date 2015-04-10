99af0d17937645de08512b01a85ff76b917563f0
unknown <HML@L0797OUL.webmedia.int>
2015-04-10 14:03:35 +0300

    Vähän pohjakoodikokeiluja.

---
+++ client/app/app.routes.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .config(route);
+        
+    route.$inject = ['$routeProvider'];
+
+    function route($routeProvider) {
+
+        $routeProvider.otherwise('/');
+
+        $routeProvider.when('/', {
+            templateUrl: 'templates/chart/chart.html',
+            controller: 'indexController as jotaki'
+        })
+        .when('/chart', {
+            templateUrl: 'templates/chart/chart.html',
+            controller: 'chartController as charter'
+        });
+
+    }     
+
+})();
---
+++ client/app/index.controller.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('indexController', index);
+
+    function index() {
+        /* jshint validthis:true */
+        var ctrl = this;
+
+        activate();
+
+        function activate() {
+
+        	ctrl.viesti = {
+        		eioo : 'YES!'
+        	};
+
+        }
+    }
+})();
